78382147e6213e3165a87b73ae7720c416ed4c6e
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-30 23:42:12 +0300

    Profile header

--- client/src/pages/Me.tsx
+++ client/src/pages/Me.tsx
@@ -2,9 +2,16 @@ import Term from "../components/Term";
 
 const Me = () => {
   return (
-    <div>
-        <h1>Me</h1>
-        <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="p-4">
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">Profile</h1>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-secondary">Add Term</button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <Term />
             <Term />
             <Term />
