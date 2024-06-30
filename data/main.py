f4901a41490f7032c28c0ba8f59351b049bc7097
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-30 17:32:52 +0300

    Requirement splitter function

--- data/main.py
+++ data/main.py
@@ -2,8 +2,18 @@ import re
 import pandas as pd
 
 # Return type is dict("prereqs": str, "coreqs": str, "antireqs": str)
-def requirement_splitter() -> dict[str, str]:
-    pass
+def requirement_splitter(requirement: str) -> dict[str, str]:
+    result = { "prereqs": "", "coreqs": "", "antireqs": "" }
+    prereqs_match = re.search(r"Prereq: ([^.]*)", requirement)
+    coreqs_match = re.search(r"Coreq: ([^.]*)", requirement)
+    antireqs_match = re.search(r"Antireq: ([^.]*)", requirement)
+    if prereqs_match:
+        result["prereqs"] = prereqs_match.group(1).strip()
+    if coreqs_match:
+        result["coreqs"] = coreqs_match.group(1).strip()
+    if antireqs_match:
+        result["antireqs"] = antireqs_match.group(1).strip()
+    return result
 
 class Course:
     def __init__(self, code, title, description, requirements):
