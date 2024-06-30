41620d02ff753a7c63470248c0993a1031178266
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-30 12:31:14 +0300

    Routing setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Me from "./pages/Me";
+
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <div className="min-h-screen">
-        App
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/me" element={<Me />} />            
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </>
